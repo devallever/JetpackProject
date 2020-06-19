032ad3b5fe3adaf895fb5ee061b30901ba837fad
devallever <devallever@163.com>
2020-06-19 17:59:49 +0800

    FEATURE:向ViewModel传递参数

--- app/src/main/java/com/allever/app/jetpack/MainActivity.kt
+++ app/src/main/java/com/allever/app/jetpack/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.allever.app.jetpack
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
@@ -8,19 +10,36 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var mViewModel: MainViewModel
+    lateinit var mSp: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        mSp = getPreferences(Context.MODE_PRIVATE)
+        val count = mSp.getInt("count", 0)
+
+        mViewModel = ViewModelProviders
+            .of(this, MainViewModelFactory(count))
+            .get(MainViewModel::class.java)
+
         btnAdd.setOnClickListener {
             mViewModel.counter++
             refreshUI()
         }
 
+        btnClear.setOnClickListener {
+            mViewModel.counter = 0
+            refreshUI()
+        }
+
         refreshUI()
 
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSp.edit().putInt("count", mViewModel.counter).apply()
     }
 
     private fun refreshUI() {
---
+++ app/src/main/java/com/allever/app/jetpack/MainViewModelFactory.kt
@@ -0,0 +1,12 @@
+package com.allever.app.jetpack
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelFactory(private val count: Int) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(count) as T
+    }
+
+}
