06e79c34a8fcb009d50b81f4751f2336ec15a707
devallever <devallever@163.com>
2020-06-19 19:20:28 +0800

    FEATURE：使用map()转换LiveData

--- app/src/main/java/com/allever/app/jetpack/MainViewModel.kt
+++ app/src/main/java/com/allever/app/jetpack/MainViewModel.kt
@@ -1,10 +1,22 @@
 package com.allever.app.jetpack
 
+import androidx.arch.core.util.Function
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class MainViewModel(count: Int) : ViewModel() {
+
+    private val userLiveData = MutableLiveData<User>()
+
+    val userName: LiveData<String> = Transformations.map(userLiveData) {
+        "${it.firstName} ${it.lastName}"
+    }
+
+//    val userName2: LiveData<String> = Transformations.map(userLiveData, Function{
+//        "${it.firstName} ${it.lastName}"
+//    })
 
     val counter: LiveData<Int>
         get() = _counter
---
+++ app/src/main/java/com/allever/app/jetpack/User.kt
@@ -0,0 +1,3 @@
+package com.allever.app.jetpack
+
+data class User(var firstName: String, var lastName: String, var age: Int)
