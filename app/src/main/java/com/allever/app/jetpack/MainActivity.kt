b804a32b7aad1e8037605e4025921c4c870c6ad7
devallever <devallever@163.com>
2020-06-19 17:50:21 +0800

    FEATURE:ViewModel基本用法

--- app/src/main/java/com/allever/app/jetpack/MainActivity.kt
+++ app/src/main/java/com/allever/app/jetpack/MainActivity.kt
@@ -2,10 +2,28 @@ package com.allever.app.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        btnAdd.setOnClickListener {
+            mViewModel.counter++
+            refreshUI()
+        }
+
+        refreshUI()
+
+    }
+
+    private fun refreshUI() {
+        tvCounter.text = mViewModel.counter.toString()
     }
 }
---
+++ app/src/main/java/com/allever/app/jetpack/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.allever.app.jetpack
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(count: Int) : ViewModel() {
+    var counter = count
+}
