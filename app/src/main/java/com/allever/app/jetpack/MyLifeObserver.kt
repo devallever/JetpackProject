7ef800d36883ae9f3bdbde0bdef8a9ad997f69ab
devallever <devallever@163.com>
2022-08-10 12:09:08 +0800

    FEATURE: LifeCycle使用推荐的方式

--- app/src/main/java/com/allever/app/jetpack/MyLifeObserver.kt
+++ app/src/main/java/com/allever/app/jetpack/MyLifeObserver.kt
@@ -1,19 +1,27 @@
 package com.allever.app.jetpack
 
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
-import com.allever.app.jetpack.ext.log
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
-class MyLifeObserver(val lifecycle: Lifecycle) : LifecycleObserver {
+class MyLifeObserver(val lifecycle: Lifecycle) : LifecycleEventObserver {
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
-    fun onActivityStart() {
-        log("onActivityStart")
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
-    fun onActivityStop() {
-        log("onActivityStop")
-    }
+//    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+//    fun onActivityStart() {
+//        log("onActivityStart")
+//    }
+//
+//    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+//    fun onActivityCreate() {
+//        log("onActivityCreate")
+//    }
+//
+//    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+//    fun onActivityStop() {
+//        log("onActivityStop")
+//    }
 }
